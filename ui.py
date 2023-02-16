0224f984e5547613ddc96c708e615bb4f0975bf3
baluR220 <1qasw23edfr45gt6@gmail.com>
2023-02-16 15:02:33 +0700

    update file

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+import tkinter as tk
 
 
 class UI():
@@ -8,10 +9,20 @@ class UI():
 
     def get_args(self):
         parser = ArgumentParser()
-        parser.add_argument('-f', help='path to yaml with data')
-        return(parser.parse_args())
+        parser.add_argument('-f', dest='file', help='path to yaml with data')
+        parser.add_argument(
+            '-v', dest='validate', action='store_true',
+            help='validate file and exit'
+        )
+        parser.add_argument(
+            '-g', dest='gui', action='store_true',
+            help='start with gui'
+        )
+        args = parser.parse_args()
+        if not (args.gui or args.file):
+            parser.print_help()
+        return(args)
 
-    
     def show_msg(self, *args, **kwargs):
         print(*args, **kwargs)
 
